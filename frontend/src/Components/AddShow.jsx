4399f9de1c709a7891272b1bf658b424ec3bca69
Suzette <suzetteislam@gmail.com>
2020-07-05 00:40:45 -0400

    form built

--- frontend/src/Components/AddShow.jsx
+++ frontend/src/Components/AddShow.jsx
@@ -2,6 +2,20 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const AddShow = () => {
+    const [allShows, setAllShows]= useState([])
+    const [newShow, setNewShow] = useState({})
+
+    useEffect(()=>{
+        const getAllShows = async () =>{
+            try{
+                let {data} = await axios.get('/shows')
+                console.log(data)
+            }catch(error){
+                console.log('error:',error)
+            }
+        }
+        getAllShows()
+    },[])
 
     return (
         <div>
