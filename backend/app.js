18c80494dfb425a7b8dab6d76de300b4dc07a9f5
Suzette <suzetteislam@gmail.com>
2020-07-04 22:14:42 -0400

    completed shows queries and imported routers to app

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const genresRouter = require('./routes/genres')
+const showsRouter = require('./routes/shows')
+const commentsRouter = require('./routes/comments')
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +19,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/genres', genresRouter)
+app.use('/shows', showsRouter)
+app.use('/comments', commentsRouter)
 
 module.exports = app;
--- backend/queries/shows.js
+++ backend/queries/shows.js
@@ -5,7 +5,7 @@ const getAllShows = async()=>{
 }
 
 const getShowsById = async(id) =>{
-    return await db.one(`SELECT * FROM shows WHERE id=$1;`)
+    return await db.one(`SELECT * FROM shows WHERE id=$1;`, id)
 }
 
 const createShow = async(title, img_url, user_id, genre_id) =>{
@@ -15,7 +15,19 @@ const createShow = async(title, img_url, user_id, genre_id) =>{
     return await db.one(insertQuery, [title, img_url, user_id, genre_id])
 }
 
+const showsByGenre = async(genre_id) =>{
+    return await db.one(`SELECT * FROM shows WHERE genre_id=$1;`, genre_id)
+}
 
+const showsByUser = async(user_id)=>{
+    return await db.any(`SELECT shows.id, title, img_url, genre_id, user_id, username, avatar_url 
+    FROM shows 
+    JOIN shows_users 
+    ON shows.id = shows_users.show_id 
+    JOIN users ON shows_users.user_id = users.id 
+    WHERE user_id = $1 
+    GROUP BY shows.id, shows_users.user_id, users.username, users.avatar_url`)
+}
 
 module.exports = {
     getAllShows, 
