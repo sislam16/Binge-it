4b3c729edac76fd9788dc3523a4b4ba4835dd269
Suzette <suzetteislam@gmail.com>
2020-07-05 00:15:13 -0400

    created nav bar, added routes to different links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom'
+import Landing from './Components/Landing';
+import Users from './Components/Users';
+import Shows from './Components/Shows';
+import AddShow from './Components/AddShow';
+import About from './Components/About'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='nav'>
+        <div className='app-name'><Link to='/'><h1>TV Watchlist App</h1></Link></div>
+        <ul>
+          <li><Link to='/users'>Users</Link></li>
+          <li><Link to='/shows'>Shows</Link></li>
+          <li><Link to='/shows/add'>Add Show</Link></li>
+          <li><Link to='/about'>About</Link></li>
+        </ul>
+
+      </div>
+
+
+      <Switch>
+        <Route exact path='/users'>
+          <Users />
+        </Route>
+
+        <Route exact path='/shows'>
+          <Shows />
+        </Route>
+
+        <Route exact path='/shows/add'>
+          <AddShow />
+        </Route>
+
+        <Route exact path='/about'>
+        <About/>
+        </Route>
+
+        <Route exact path='/'>
+          <Landing />
+        </Route>
+      </Switch>
     </div>
   );
 }
