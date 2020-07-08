f757f176f02f0473756339a70bd781cda96dd00c
Suzette <suzetteislam@gmail.com>
2020-07-08 13:48:02 -0400

    created cards for shows

--- frontend/src/Components/ShowCard.jsx
+++ frontend/src/Components/ShowCard.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const ShowsCard = () =>{
-    return(
-        <div>
-            "ShowsCard"
+const ShowsCard = ({ show_img, title, genre }) => {
+    return (
+        <div className='shows-card'>
+            <img src={show_img} />
+            <div className='shows-card-text'>
+                <h2>{title}</h2>
+                <p>{genre}</p>
+            </div>
+
         </div>
     )
 }
