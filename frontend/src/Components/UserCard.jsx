85438a0668e5cf9e68ec2a563bc58759bacc6a03
Suzette <suzetteislam@gmail.com>
2020-07-07 20:54:53 -0400

    built user card for all users page

--- frontend/src/Components/UserCard.jsx
+++ frontend/src/Components/UserCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-const UserCard = () =>{
-    return(
-        <div>
-            UserCard
+const UserCard = ({ img, name, id }) => {
+    return (
+        <div className='user-card' style={{display:'flex', flexDirection:'row',justifyContent:'center', marginBottom:'5%'}}>
+            <img src={img}  style={{height:'100px'}}/>
+            <Link to={`/users/${id}`}> <h2>{name}</h2></Link>
         </div>
+        
     )
 }
 
