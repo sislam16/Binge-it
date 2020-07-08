709804425269b5af06d27f77f9f14847642728b6
Suzette <suzetteislam@gmail.com>
2020-07-08 13:48:26 -0400

    all shows populate on shows page

--- frontend/src/Components/Shows.jsx
+++ frontend/src/Components/Shows.jsx
@@ -1,10 +1,34 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import ShowsCard from './ShowCard'
 
-const Shows = () =>{
-    return(
+const Shows = () => {
+    const [allShows, setAllShows] = useState([])
+
+    useEffect(() => {
+        const getAllShows = async () => {
+            try {
+                let { data } = await axios.get('/shows')
+                setAllShows(data.payload)
+                console.log('shows', data.payload)
+            } catch (error) {
+                console.log('error', error)
+            }
+        }
+        getAllShows()
+    }, [])
+
+    const showsThumbnails = allShows.map(el=>
+        <ShowsCard
+        title={el.title}
+        show_img={el.img_url}
+        genre={el.genre_id}
+        />)
+
+    return (
         <div>
             <h1>Shows</h1>
+            {showsThumbnails}
         </div>
     )
 }
