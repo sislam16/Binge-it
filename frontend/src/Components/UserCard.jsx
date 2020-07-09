59ede20851f2919649d47d64d5aa58e8a4791f6b
Suzette <suzetteislam@gmail.com>
2020-07-08 23:35:09 -0400

    added id to card

--- frontend/src/Components/UserCard.jsx
+++ frontend/src/Components/UserCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import UserProfile from './UserProfile'
 
 const UserCard = ({ img, name, id }) => {
     return (
-        <div className='user-card' style={{display:'flex', flexDirection:'row',justifyContent:'center', marginBottom:'5%'}}>
+        <div className='user-card' id={id} style={{display:'flex', flexDirection:'row',justifyContent:'center', marginBottom:'5%'}}>
             <img src={img}  style={{height:'100px'}}/>
             <Link to={`/users/${id}`}> <h2>{name}</h2></Link>
         </div>
