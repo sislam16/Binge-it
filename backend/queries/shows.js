7ed3dbe5cc1a48df64b1bfce66a3469739ca1dea
Suzette <suzetteislam@gmail.com>
2020-07-09 00:06:09 -0400

    get all shows query has a join on genres

--- backend/queries/shows.js
+++ backend/queries/shows.js
@@ -1,25 +1,25 @@
 const db = require('../database/db')
 
-const getAllShows = async()=>{
-    return await db.any(`SELECT * FROM shows;`)
+const getAllShows = async () => {
+    return await db.any(`SELECT * FROM shows JOIN genres ON shows.genre_id=genres.id;`)
 }
 
-const getShowsById = async(id) =>{
+const getShowsById = async (id) => {
     return await db.one(`SELECT * FROM shows WHERE id=$1;`, id)
 }
 
-const createShow = async(title, img_url, user_id, genre_id) =>{
+const createShow = async (title, img_url, user_id, genre_id) => {
     const insertQuery = `INSERT INTO shows(title, img_url, user_id, genre_id)
     VALUES($1, $2, $3, $4)
     RETURNING *`
     return await db.one(insertQuery, [title, img_url, user_id, genre_id])
 }
 
-const showsByGenre = async(genre_id) =>{
+const showsByGenre = async (genre_id) => {
     return await db.one(`SELECT * FROM shows WHERE genre_id=$1;`, genre_id)
 }
 
-const showsByUser = async(user_id)=>{
+const showsByUser = async (user_id) => {
     return await db.any(`SELECT shows.id, title, img_url, genre_id, user_id, username, avatar_url 
     FROM shows 
     JOIN shows_users 
@@ -30,7 +30,7 @@ const showsByUser = async(user_id)=>{
 }
 
 module.exports = {
-    getAllShows, 
-    getShowsById, 
+    getAllShows,
+    getShowsById,
     createShow
 }
