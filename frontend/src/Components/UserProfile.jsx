1ab7a27d3e48cb2232e06d98c4b7cf41f01f9a1f
Suzette <suzetteislam@gmail.com>
2020-07-08 23:35:26 -0400

    populating user profile

--- frontend/src/Components/UserProfile.jsx
+++ frontend/src/Components/UserProfile.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link, useParams } from 'react-router-dom'
 
-const UserProfile = () =>{
-    return(
+const UserProfile = () => {
+    const [userInfo, setUserInfo] = useState([])
+    const { id } = useParams()
+    console.log('id in the front', id)
+
+    useEffect(() => {
+        const currentUser = async () => {
+            try {
+                let { data } = await axios.get(`/users/${id}`)
+                setUserInfo(data.payload)
+            } catch (error) {
+                console.log('error', error)
+            }
+        }
+        currentUser()
+    }, [id])
+
+    console.log('usr:', userInfo)
+
+    return (
         <div>
-            UserProfile
+            <h2>{userInfo.username} Profile</h2>
+            <div className='usr-shows'>
+                
+            </div>
         </div>
     )
 }
