51e8b1afe23138184fe445097e8a41bb3f7e2d69
Suzette <suzetteislam@gmail.com>
2020-07-09 00:04:53 -0400

    populated shows and genre dropdown menu

--- frontend/src/Components/AddShow.jsx
+++ frontend/src/Components/AddShow.jsx
@@ -2,27 +2,52 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const AddShow = () => {
-    const [allShows, setAllShows]= useState([])
+    const [allShows, setAllShows] = useState([])
     const [newShow, setNewShow] = useState({})
+    const [allGenres, setAllGenres] = useState([])
 
-    useEffect(()=>{
-        const getAllShows = async () =>{
-            try{
-                let {data} = await axios.get('/shows')
-                console.log(data)
-            }catch(error){
-                console.log('error:',error)
+    useEffect(() => {
+        const getAllShows = async () => {
+            try {
+                let { data } = await axios.get('/shows')
+                setAllShows(data.payload)
+                console.log('shows', data)
+            } catch (error) {
+                console.log('error:', error)
             }
         }
         getAllShows()
-    },[])
+    }, [])
+
+    useEffect(() => {
+        const getAllGenres = async () => {
+            try {
+                let { data } = await axios.get('/genres')
+                setAllGenres(data.payload)
+                console.log('genres', data)
+            } catch (error) {
+                console.log('error', error)
+            }
+        }
+        getAllGenres()
+    }, [])
+
+    const showOptions = allShows.map(el =>
+        <option>{el.title}</option>
+    )
+    
+    const genreOptions = allGenres.map(el=>
+        <option>{el.genre_name}</option>)
 
     return (
         <div>
             <h1>Add Show</h1>
             <form className='existing'>
                 <h3>Start Watching Show</h3>
-                <select></select><br />
+                <select>
+                    <option>---All Shows---</option>
+                    {showOptions}
+                </select><br />
                 <button>Start Watching</button><br />
             </form>
 
@@ -33,7 +58,10 @@ const AddShow = () => {
                 <label> Show Name</label><br />
                 <input type="text" placeholder='Name' /><br />
                 <label>Genre</label> <br />
-                <select name="" id=""></select><br />
+                <select>
+                    <option>---Select a Genre---</option>
+                    {genreOptions}
+                    </select><br />
                 <button>Add</button>
             </form>
         </div>
