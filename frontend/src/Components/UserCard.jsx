b8585bb120176dd57f30628f44a66829cc9deae2
Suzette <suzetteislam@gmail.com>
2020-07-12 14:31:35 -0400

    added alt to img

--- frontend/src/Components/UserCard.jsx
+++ frontend/src/Components/UserCard.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import UserProfile from './UserProfile'
 
 const UserCard = ({ img, name, id }) => {
     return (
         <div className='user-card' id={id} style={{display:'flex', flexDirection:'row',justifyContent:'center', marginBottom:'5%'}}>
-            <img src={img}  style={{height:'100px'}}/>
+            <img src={img}  style={{height:'100px'}} alt ='usr-img'/>
             <Link to={`/users/${id}`}> <h2>{name}</h2></Link>
         </div>
         
