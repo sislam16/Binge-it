7881266915114aebe94abcc8937e27e3fcfa5638
Suzette <suzetteislam@gmail.com>
2020-07-12 02:26:35 -0400

    show info populates, comments load. Still need to display users watching and functionality to add comment form

--- frontend/src/Components/WatchShow.jsx
+++ frontend/src/Components/WatchShow.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 
 
 const WatchShow = () => {
+    const [comments, setComments] = useState([])
     const [currentShow, setCurrentShow] = useState({})
     const { show_id } = useParams()
 
@@ -11,20 +12,51 @@ const WatchShow = () => {
         const getSelectedShow = async () => {
             try {
                 let { data } = await axios.get(`/shows/${show_id}`)
-                setCurrentShow(data.payload)
+                setCurrentShow(data.payload[0])
             } catch (error) {
                 console.log('error', error)
             }
         }
         getSelectedShow()
+    }, [show_id])
+
+    console.log(currentShow)
+    useEffect(() => {
+        const getShowComments = async () => {
+            try {
+                let { data } = await axios.get(`/comments/show/${show_id}`)
+                setComments(data.payload)
+            } catch (error) {
+                console.log('err:', error)
+            }
+        }
+        getShowComments()
     }, [])
 
+    console.log('comments', comments)
+    // console.log('currShow:', currentShow)
 
+    const showComments = comments.map(el =>
+        <div>
+            <p style={{fontWeight:'bold'}}>{el.username}</p>
+            <p>{el.comment_body}</p>
+        </div>
+    )
+    
     return (
         <div>
-            <h2>{currentShow.title}</h2>
-            <img src={currentShow.img_url} alt='poster'/>
-            <p></p>
+            <div>
+                <h2>{currentShow.title}</h2>
+                <img src={currentShow.img_url} alt='poster' />
+                <p>{currentShow.genre_name}</p>
+            </div>
+
+            <div><p>Watched by:</p></div>
+
+            <form>
+                <input type='text' placeholder='comment' /> <button>Add</button>
+            </form>
+            {showComments}
         </div>
     )
 }
