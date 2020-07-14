d6bc1ef0745a9417a81420c0f88e1966e90fd938
Suzette <suzetteislam@gmail.com>
2020-07-14 18:24:41 -0400

    card file update

--- frontend/src/Components/ShowCard.jsx
+++ frontend/src/Components/ShowCard.jsx
@@ -1,35 +1,34 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 const ShowsCard = ({ show_img, title, genre, id }) => {
-    const [watchers, setWatchers] =useState([])
+    const [watchers, setWatchers] = useState([])
 
-    useEffect(() =>{
-        const getWatchers = async()=>{
-            try{
-                let {data} = await axios.get(`/shows/watchers/${id}`)
+    useEffect(() => {
+        const getWatchers = async () => {
+            try {
+                let { data } = await axios.get(`/shows/watchers/${id}`)
                 console.log(data.payload)
                 setWatchers(data.payload)
-            } catch(error){
+            } catch (error) {
                 console.log('err:', error)
             }
         }
         getWatchers()
     }, [id])
-    console.log('oop', watchers)
 
-    const displayWatchers = watchers.map(el=>
+    const displayWatchers = watchers.map(el =>
         JSON.parse(JSON.stringify(el.username)))
-        console.log(displayWatchers)
     const displayUsername = displayWatchers.join(', ')
+
     return (
         <div className='shows-card'>
-            <img src={show_img} alt='show-poster' />
+            <img src={show_img} alt='show-poster' className='shows-poster' />
             <div className='shows-card-text'>
-             <Link to={`/shows/${id}`}><h2>{title}</h2></Link>   
+                <Link to={`/shows/${id}`}><h3>{title}</h3></Link>
                 <p>{genre}</p>
-    <span style={{fontWeight:'bold'}}>Being watched by: </span>{displayUsername}
+                <span style={{ fontWeight: 'bold' }}>Being watched by: </span>{displayUsername}
             </div>
 
         </div>
